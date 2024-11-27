ae1e251a430bf5cf3c326f6eee50e9012a998194
Lochinbek Abdiyev <94645066+lochinbek02@users.noreply.github.com>
2024-11-27 18:55:12 +0500

    new commit

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+(cgc61k2b$eb3_fbz)wrqh58jhvdvu%3^_%653if1=(24+zut'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG",default=False)
+DEBUG = False
 ENVIRONMENT = env.str('ENVIRONMENT', default='development')
 ALLOWED_HOSTS = ['*']
 # INTERNAL_IPS=(
